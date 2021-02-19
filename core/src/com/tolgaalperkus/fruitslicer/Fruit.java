e609bc438a33a95277c8efb6084a4539706c2cf9
tolga <tolgaalperkus@hotmail.com>
2021-02-19 15:47:17 +0300

    first steps

---
+++ core/src/com/tolgaalperkus/fruitslicer/Fruit.java
@@ -0,0 +1,5 @@
+package com.tolgaalperkus.fruitslicer;
+
+public class Fruit {
+
+}
--- core/src/com/tolgaalperkus/fruitslicer/FruitSlicer.java
+++ core/src/com/tolgaalperkus/fruitslicer/FruitSlicer.java
@@ -9,18 +9,23 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.utils.TimeUtils;
 
 //BU KISIMDA GEREKLİ GDX KOMUTLARI BULUNMAKTA OYUNUN TEMELİ BURASI
 
 //inputların takip edilebilmesi için InputProcessor interface'ini implement ediyoruz
 public class FruitSlicer extends ApplicationAdapter implements InputProcessor {
-	int gameState = 0;
+
 	SpriteBatch batch;
 	//Kullanacağımız imajları burada Texture sınıfında tanımlıyoruz.
-	Texture background,carrot,cherry,apple,strawberry,bomb,time;
+	Texture background,carrot,cherry,apple,strawberry,bomb,time,coin;
 	//Menü başlangıcı ve skor kısımlarındaki yazı türünü belirtiyorum
 	BitmapFont font;
 	FreeTypeFontGenerator fontGen;
+	int lives = 4;
+	int score = 0;
+	private double currentTime = 0;
+	private double gameOverTime = -1.0f;
 
 	@Override
 	public void create () {
@@ -33,6 +38,8 @@ public class FruitSlicer extends ApplicationAdapter implements InputProcessor {
 		strawberry = new Texture("strawberry.png");
 		bomb = new Texture("piecesBomb.png");
 		time = new Texture("piecesTime.png");
+		coin = new Texture("piecesCoin.png");
+
 		//Buradaki aktivitenin kullanıcıdan gelen girdileri işleyeceğini belirtiyoruz.
 		Gdx.input.setInputProcessor(this);
 		//Aşağıda kullanacağımız fontu ve onun özelliklerini FreeType eklentisi sayesinde
@@ -52,13 +59,24 @@ public class FruitSlicer extends ApplicationAdapter implements InputProcessor {
 		batch.begin();
 		//Gdx.graphics.getWidth() ve Gdx.graphics.getHeight() komutlarıyla cihaz ekranı boyutlarını çağırıyoruz
 		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		//farklı cihazlarda saniye başına düşen frame sayısını düzenliyoruz
+		double newTime = TimeUtils.millis()/1000.0;
+		
+		if (lives <= 0 && gameOverTime == 0f)
+		{
+			//oyun bitimi
+			gameOverTime = currentTime;
+		}
+		if(lives > 0)
+		{
+			//oyun çalışırken yapılacaklar
+			for (int i=0;i<lives;i++){
+				batch.draw(coin,30+(i*100),Gdx.graphics.getHeight()-110f,100f,100f);
+			}
+		}
+
 		//ekrana yazacağımız yazıları ekliyoruz
-		if(gameState==0){
-			font.draw(batch,"Cut To Play",Gdx.graphics.getWidth()/2f-250,Gdx.graphics.getHeight()/2f);
-		}
-		else if(gameState==1){
-			font.draw(batch,"Score :",50,100);
-		}
+		font.draw(batch,"Score :",50,100);
 		font.draw(batch,"Cut To Play",Gdx.graphics.getWidth()/2f-250,Gdx.graphics.getHeight()/2f);
 		batch.end();
 	}
